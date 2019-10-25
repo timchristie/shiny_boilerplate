a770e4d3ad74a8a537693235f731ca2e44103030
timchristie <timchristie92@gmail.com>
2019-10-24 23:32:58 -0700

    initial commit

---
+++ misc/menu.R
@@ -0,0 +1,12 @@
+sidebarMenu(
+  menuItem(
+    "Example 1",
+    tabName = "example1",
+    icon = icon("user-circle-o")
+  ),
+  menuItem(
+    "Example 2",
+    tabName = "example2",
+    icon = icon("user-circle-o")
+  )
+)
---
+++ tabs/example1/frontend.R
@@ -0,0 +1,9 @@
+sidebarLayout(
+  sidebarPanel = box(
+    title = 'Example 1',
+    HTML('Hello world!'),
+    width = 4,
+    status = 'primary'
+  ),
+  mainPanel = HTML('')
+)
